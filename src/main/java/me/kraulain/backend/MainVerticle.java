981548589ffe5d088aa5afe2f945d7bfe8b21f5e
Fosso Arcel <kraulain@gmail.com>
2018-12-19 02:31:12 +0100

    add code to connect to db and start http server

--- src/main/java/me/kraulain/backend/MainVerticle.java
+++ src/main/java/me/kraulain/backend/MainVerticle.java
@@ -2,23 +2,114 @@ package me.kraulain.backend;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CorsHandler;
+import me.kraulain.backend.handlers.GetAllArticlesHandler;
+import me.kraulain.backend.handlers.HealthCheckHandler;
+import me.kraulain.backend.handlers.ResourceNotFoundHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private JDBCClient dbClient;
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+  private static final String SQL_CREATE_ARTICLE_TABLE = "create table if not exists article (Id integer identity primary key, Name varchar(255) unique, Content clob)";
+
   @Override
   public void start(Future<Void> startFuture) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8080, http -> {
-      if (http.succeeded()) {
-        startFuture.complete();
-        System.out.println("HTTP server started on http://localhost:8080");
+    Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
+    steps.setHandler(startFuture.completer());
+  }
+
+  private Future<Void> prepareDatabase() {
+    Future<Void> future = Future.future();
+    dbClient = JDBCClient.createShared(vertx, new JsonObject()
+      .put("url", "jdbc:hsqldb:file:db/blog")
+      .put("driver_class", "org.hsqldb.jdbcDriver")
+      .put("max_pool_size", 30));
+
+    dbClient.getConnection(ar -> {
+      if (ar.failed()) {
+        LOGGER.error("Could not open a database connection", ar.cause());
+        future.fail(ar.cause());
       } else {
-        startFuture.fail(http.cause());
+        SQLConnection connection = ar.result();
+        connection.execute(SQL_CREATE_ARTICLE_TABLE, create -> {
+          connection.close();
+          if (create.failed()) {
+            LOGGER.error("Database preparation error", create.cause());
+            future.fail(create.cause());
+          } else {
+            future.complete();
+          }
+        });
       }
     });
+    return future;
+  }
+
+  private Future<Void> startHttpServer() {
+    Future<Void> future = Future.future();
+    HttpServer server = vertx.createHttpServer();
+
+    LOGGER.debug("in main.HttpVerticle.start(..)");
+
+    Router router = Router.router(vertx);
+    router.route()
+      .handler(
+        CorsHandler.create("*")
+          .allowedMethod(HttpMethod.GET)
+          .allowedMethod(HttpMethod.POST)
+          .allowedMethod(HttpMethod.PUT)
+          .allowedMethod(HttpMethod.OPTIONS)
+          .allowedHeader(HttpHeaders.ACCEPT.toString())
+          .allowedHeader(HttpHeaders.CONTENT_TYPE.toString()));
+
+    // Handlers
+    router.route().handler(BodyHandler.create());
+
+    // Health Check
+    router.get("/health")
+      .handler(new HealthCheckHandler());
+
+    // Error
+    router.route()
+      .last()
+      .handler(new ResourceNotFoundHandler());
+
+    // blog endpoint
+    router.mountSubRouter("/blog", blogRoutes());
+
+    server.requestHandler(router::accept)
+      .listen(8888, ar -> {
+        if (ar.succeeded()) {
+          LOGGER.info("HTTP server running on port 8888");
+          future.complete();
+        } else {
+          LOGGER.error("Could not start a HTTP server", ar.cause());
+          future.fail(ar.cause());
+        }
+      });
+    return future;
+  }
+
+  private Router blogRoutes() {
+    LOGGER.debug("Mounting '/blog' endpoint");
+
+    Router router = Router.router(vertx);
+    //Get all articles paginated
+    router.get("/articles").handler(new GetAllArticlesHandler());
+
+    return router;
   }
 
 }
