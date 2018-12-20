aeab41674f860375711bf651949d6c951e9da5f6
Fosso Arcel <kraulain@gmail.com>
2018-12-21 00:11:16 +0100

    create user entity

---
+++ src/main/java/me/kraulain/backend/entities/User.java
@@ -0,0 +1,65 @@
+package me.kraulain.backend.entities;
+
+import java.util.Objects;
+
+public class User {
+  private int id;
+  private String name;
+  private String email;
+
+  public User(){}
+
+  public User(int id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      ", email='" + email + '\'' +
+      '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return getId() == user.getId() &&
+      Objects.equals(getName(), user.getName()) &&
+      Objects.equals(getEmail(), user.getEmail());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getName(), getEmail());
+  }
+}
