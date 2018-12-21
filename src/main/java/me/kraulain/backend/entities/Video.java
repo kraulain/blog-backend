fe7bdae22df92835d1ab61cc2fd2ae9a0d8832ad
Fosso Arcel <kraulain@gmail.com>
2018-12-22 00:31:50 +0100

    encapsulate properties of Video entity

--- src/main/java/me/kraulain/backend/entities/Video.java
+++ src/main/java/me/kraulain/backend/entities/Video.java
@@ -1,10 +1,58 @@
 package me.kraulain.backend.entities;
 
 public class Video {
-  int id;
-  String title;
-  String subTitle;
-  String youtubeUrl;
-  String description;
-  String Status = "draft"; // published, deleted
+  private int id;
+  private String title;
+  private String subTitle;
+  private String youtubeUrl;
+  private String description;
+  private String Status = "draft"; // published, deleted
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getSubTitle() {
+    return subTitle;
+  }
+
+  public void setSubTitle(String subTitle) {
+    this.subTitle = subTitle;
+  }
+
+  public String getYoutubeUrl() {
+    return youtubeUrl;
+  }
+
+  public void setYoutubeUrl(String youtubeUrl) {
+    this.youtubeUrl = youtubeUrl;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getStatus() {
+    return Status;
+  }
+
+  public void setStatus(String status) {
+    Status = status;
+  }
 }
