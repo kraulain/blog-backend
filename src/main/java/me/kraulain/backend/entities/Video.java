8121c201d8ba5d1c25f4404fd333e66db45035ba
Fosso Arcel <kraulain@gmail.com>
2018-12-22 00:30:44 +0100

    add properties video entity

--- src/main/java/me/kraulain/backend/entities/Video.java
+++ src/main/java/me/kraulain/backend/entities/Video.java
@@ -1,5 +1,10 @@
 package me.kraulain.backend.entities;
 
 public class Video {
-
+  int id;
+  String title;
+  String subTitle;
+  String youtubeUrl;
+  String description;
+  String Status = "draft"; // published, deleted
 }
