d249d063fb8a1af88f9b8f49120c9b7db6317561
Fosso Arcel <kraulain@gmail.com>
2019-01-30 23:43:09 +0100

    Add query to create podcast table

--- src/main/java/me/kraulain/backend/MainVerticle.java
+++ src/main/java/me/kraulain/backend/MainVerticle.java
@@ -282,6 +282,8 @@ public class MainVerticle extends AbstractVerticle {
       "occurrence_date date, cause varchar(255), error_message varchar(255),status varchar(15))";
     String CREATE_MESSAGE_TABLE = "create table if not exists article (id integer identity primary key, " +
       "email varchar(50), name varchar(100), message clob, published_date date, status varchar(15), language varchar(3))";
+    String CREATE_PODCAST_TABLE = "create table if not exists article (id integer identity primary key, " +
+      "title varchar(100), sub_title varchar(100), image_url varchar(255), soundcloud_url varchar(255), description clob, status varchar(15), language varchar(3))";
     createAllTables.add(CREATE_ARTICLE_TABLE);
     createAllTables.add(CREATE_APP_TABLE);
     createAllTables.add(CREATE_BOOK_TABLE);
@@ -290,6 +292,7 @@ public class MainVerticle extends AbstractVerticle {
     createAllTables.add(CREATE_EMAIL_TABLE);
     createAllTables.add(CREATE_ISSUE_TABLE);
     createAllTables.add(CREATE_MESSAGE_TABLE);
+    createAllTables.add(CREATE_PODCAST_TABLE);
   }
 
 }
