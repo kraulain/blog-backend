5531643510946f5c25889c679ed1175c911916ca
Fosso Arcel <kraulain@gmail.com>
2019-02-09 00:41:39 +0100

    pass db client to constructor of getapphandler

--- src/main/java/me/kraulain/backend/handlers/blog/app/GetAppHandler.java
+++ src/main/java/me/kraulain/backend/handlers/blog/app/GetAppHandler.java
@@ -5,13 +5,21 @@ import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.web.RoutingContext;
+import me.kraulain.backend.dao.AppDAO;
 import me.kraulain.backend.responses.MediaTypes;
 
 import java.net.HttpURLConnection;
 
 public class GetAppHandler implements Handler<RoutingContext> {
   private final static Logger LOGGER = LoggerFactory.getLogger(GetAppHandler.class);
+
+  private AppDAO appDAO;
+
+  public GetAppHandler(JDBCClient dbClient) {
+    appDAO = new AppDAO(dbClient);
+  }
 
     @Override
     public void handle(RoutingContext routingContext) {
