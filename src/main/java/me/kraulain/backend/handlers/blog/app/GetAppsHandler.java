66d6daf919cb33047e9302f8bacdd894d843547e
Fosso Arcel <kraulain@gmail.com>
2019-02-08 23:59:50 +0100

    implement getAppsHandler logic

--- src/main/java/me/kraulain/backend/handlers/blog/app/GetAppsHandler.java
+++ src/main/java/me/kraulain/backend/handlers/blog/app/GetAppsHandler.java
@@ -2,6 +2,7 @@ package me.kraulain.backend.handlers.blog.app;
 
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -11,27 +12,41 @@ import me.kraulain.backend.dao.AppDAO;
 import me.kraulain.backend.responses.MediaTypes;
 
 import java.net.HttpURLConnection;
+import java.util.List;
 
 public class GetAppsHandler implements Handler<RoutingContext> {
   private final static Logger LOGGER = LoggerFactory.getLogger(GetAppsHandler.class);
   private AppDAO appDAO;
 
-  public GetAppsHandler(JDBCClient dbClient){
+  public GetAppsHandler(JDBCClient dbClient) {
     appDAO = new AppDAO(dbClient);
   }
 
-    @Override
-    public void handle(RoutingContext routingContext) {
-      LOGGER.debug("get all apps {}",
-        routingContext.request()
-          .absoluteURI());
+  @Override
+  public void handle(RoutingContext routingContext) {
+    LOGGER.debug("get all apps {}",
+      routingContext.request()
+        .absoluteURI());
 
-      JsonObject response = new JsonObject();
-      response.put("greeting", "Hello from get all apps handler");
+    List<JsonArray> apps = appDAO.selectAll();
 
+    JsonObject response = new JsonObject();
+
+    if (!apps.equals(null)) {
+      response.put("title", "Wiki home");
+      response.put("apps", apps);
       routingContext.response()
         .setStatusCode(HttpURLConnection.HTTP_OK)
         .putHeader(HttpHeaders.CONTENT_TYPE, MediaTypes.APPLICATION_JSON)
         .end(response.encode());
+    } else {
+      response.put("error", "Something went wrong");
+      routingContext.response()
+        .setStatusCode(HttpURLConnection.HTTP_INTERNAL_ERROR)
+        .putHeader(HttpHeaders.CONTENT_TYPE, MediaTypes.APPLICATION_JSON)
+        .end(response.encode());
     }
+
+
+  }
 }
