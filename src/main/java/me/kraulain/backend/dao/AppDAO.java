75b4bbb1e177ef28176f675c3801bfce6ec3a459
Fosso Arcel <kraulain@gmail.com>
2019-02-08 23:44:00 +0100

    implement select all apps dao method

--- src/main/java/me/kraulain/backend/dao/AppDAO.java
+++ src/main/java/me/kraulain/backend/dao/AppDAO.java
@@ -1,12 +1,39 @@
 package me.kraulain.backend.dao;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+import me.kraulain.backend.entities.App;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AppDAO {
-  private JDBCClient dbClient;
 
-  public AppDAO(JDBCClient dbClient){
+  private JDBCClient dbClient;
+  private String SELECT_ALL_APPS = "select * from app";
+
+  public AppDAO(JDBCClient dbClient) {
     this.dbClient = dbClient;
+  }
+
+  public List<JsonArray> selectAll() {
+
+    final AtomicReference<List<JsonArray>> appsReference = new AtomicReference<>();
+
+    dbClient.getConnection(ar -> {
+      if (ar.succeeded()) {
+        SQLConnection connection = ar.result();
+        connection.query(SELECT_ALL_APPS, res -> {
+          connection.close();
+          if (res.succeeded()) {
+            appsReference.set(res.result().getResults());
+          }
+        });
+      }
+    });
+
+    return appsReference.get();
   }
 
   //Todo: select all or search
