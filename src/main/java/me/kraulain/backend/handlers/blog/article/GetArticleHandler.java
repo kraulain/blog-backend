c1e9f558b2d49b6161cc7d52db791946695f5bae
Fosso Arcel <kraulain@gmail.com>
2019-02-27 15:22:21 +0100

    wire up get article handler

--- src/main/java/me/kraulain/backend/handlers/blog/article/GetArticleHandler.java
+++ src/main/java/me/kraulain/backend/handlers/blog/article/GetArticleHandler.java
@@ -5,7 +5,9 @@ import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.web.RoutingContext;
+import me.kraulain.backend.dao.ArticleDAO;
 import me.kraulain.backend.responses.MediaTypes;
 
 import java.net.HttpURLConnection;
@@ -13,18 +15,19 @@ import java.net.HttpURLConnection;
 public class GetArticleHandler implements Handler<RoutingContext> {
   private final static Logger LOGGER = LoggerFactory.getLogger(GetArticleHandler.class);
 
+  private ArticleDAO articleDAO;
+
+  public GetArticleHandler(JDBCClient dbClient) {
+    articleDAO = new ArticleDAO(dbClient);
+  }
+
     @Override
     public void handle(RoutingContext routingContext) {
       LOGGER.debug("get article {}",
         routingContext.request()
           .absoluteURI());
 
-      JsonObject response = new JsonObject();
-      response.put("greeting", "Hello from  get single article handler");
-
-      routingContext.response()
-        .setStatusCode(HttpURLConnection.HTTP_OK)
-        .putHeader(HttpHeaders.CONTENT_TYPE, MediaTypes.APPLICATION_JSON)
-        .end(response.encode());
+      String id = routingContext.request().getParam("id");
+      articleDAO.selectById(routingContext, Integer.valueOf(id));
     }
 }
