75a82b717ef71ffa4769a72e13e2dc95a24bac86
Fosso Arcel <kraulain@gmail.com>
2019-03-05 23:29:38 +0100

    wire up post article handler to article dao

--- src/main/java/me/kraulain/backend/handlers/blog/article/PostArticleHandler.java
+++ src/main/java/me/kraulain/backend/handlers/blog/article/PostArticleHandler.java
@@ -5,13 +5,21 @@ import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.web.RoutingContext;
+import me.kraulain.backend.dao.ArticleDAO;
+import me.kraulain.backend.entities.Article;
 import me.kraulain.backend.responses.MediaTypes;
 
 import java.net.HttpURLConnection;
 
 public class PostArticleHandler implements Handler<RoutingContext> {
   private final static Logger LOGGER = LoggerFactory.getLogger(PostArticleHandler.class);
+  private ArticleDAO articleDAO;
+
+  public PostArticleHandler(JDBCClient dbClient) {
+    articleDAO = new ArticleDAO(dbClient);
+  }
 
     @Override
     public void handle(RoutingContext routingContext) {
@@ -19,12 +27,7 @@ public class PostArticleHandler implements Handler<RoutingContext> {
         routingContext.request()
           .absoluteURI());
 
-      JsonObject response = new JsonObject();
-      response.put("greeting", "Hello from post article handler");
-
-      routingContext.response()
-        .setStatusCode(HttpURLConnection.HTTP_OK)
-        .putHeader(HttpHeaders.CONTENT_TYPE, MediaTypes.APPLICATION_JSON)
-        .end(response.encode());
+      Article article = routingContext.getBodyAsJson().mapTo(Article.class);
+      articleDAO.insert(routingContext, article);
     }
 }
